a6cce6eaeb00fefc870e47fcf83ee67d7300ff35
arifkk786 <mohammedarifkk@gamil.com>
2021-01-22 15:24:42 +0000

    commit

--- Day 5 data structures.R
+++ Day 5 data structures.R
@@ -4,3 +4,9 @@ vector("character", length=5)
 a=c("as","fgh")
 typeof(a)
 #as.integer to convert numeric to integer
+#length(),class(),str(),typeof()
+series <- 1:100
+series
+ser1=seq(100)
+ser1
+#merged.list=c(list1,list2)
